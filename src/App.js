43790f31aa20398ae61feabc67f546501fd2e8b0
Maksym Pecherytsia <findimrk@gmail.com>
2023-03-20 15:46:05 +0200

    moved clarifai api from clietn side to server side, plus refactored code

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import Clarifai from "clarifai";
 import FaceRecognition from "./components/FaceRecognition/FaceRecognition";
 import Navigation from "./components/Navigation/Navigation";
 import Signin from "./components/Signin/Signin";
@@ -10,32 +9,31 @@ import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
 import Rank from "./components/Rank/Rank";
 import ParticlesBg from "particles-bg";
 
-const app = new Clarifai.App({
-	apiKey: "dbebc45a48b34819abf37587742c12e8",
-});
 
 // https://images.unsplash.com/photo-1530099486328-e021101a494a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1547&q=80
 // https://images.unsplash.com/photo-1508214751196-bcfd4ca60f91?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80
 //https://images.unsplash.com/photo-1522850403397-b0c8f2f75451?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80
 // https://myfacelady.com/wp-content/uploads/elementor/thumbs/Dermal-Fillers-scaled-ptauziv7b066by5yquig7thlakfd6x3gcvwlibngy0.jpeg
 
+const initialState = {
+	input: "",
+	imageUrl: "",
+	box: {},
+	route: "signin",
+	isSignedIn: false,
+	user: {
+		id: "",
+		name: "",
+		email: "",
+		entries: 0,
+		joined: "",
+	},
+};
+
 class App extends Component {
 	constructor() {
 		super();
-		this.state = {
-			input: "",
-			imageUrl: "",
-			box: {},
-			route: "signin",
-			isSignedIn: false,
-			user: {
-				id: "",
-				name: "",
-				email: "",
-				entries: 0,
-				joined: "",
-			},
-		};
+		this.state = initialState;
 	}
 
 	loadUser = (data) => {
@@ -51,17 +49,31 @@ class App extends Component {
 	};
 
 	calculateFaceLocation = (data) => {
-		const clarifaiFace =
-			data.outputs[0].data.regions[0].region_info.bounding_box;
 		const image = document.getElementById("inputImage");
+		
 		const width = Number(image.width);
 		const height = Number(image.height);
-		return {
-			leftCol: clarifaiFace.left_col * width,
-			topRow: clarifaiFace.top_row * height,
-			rightCol: width - clarifaiFace.right_col * width,
-			bottomRow: height - clarifaiFace.bottom_row * height,
-		};
+
+		// const clarifaiFace =
+		// 	data.outputs[0].data.regions[0].region_info.bounding_box;
+
+		return data.outputs[0].data.regions.map((regions) => {
+			const clarifaiFace = regions.region_info.bounding_box;
+			return {
+				leftCol: clarifaiFace.left_col * width,
+				topRow: clarifaiFace.top_row * height,
+				rightCol: width - clarifaiFace.right_col * width,
+				bottomRow: height - clarifaiFace.bottom_row * height,
+			};
+		});
+		
+
+		// return {
+		// 	leftCol: clarifaiFace.left_col * width,
+		// 	topRow: clarifaiFace.top_row * height,
+		// 	rightCol: width - clarifaiFace.right_col * width,
+		// 	bottomRow: height - clarifaiFace.bottom_row * height,
+		// };
 	};
 
 	displayFaceBox = (box) => {
@@ -74,17 +86,24 @@ class App extends Component {
 
 	onButtonSubmit = () => {
 		this.setState({ imageUrl: this.state.input });
-
-		app.models
-			.predict(
-				{
-					id: "face-detection",
-					name: "face-detection",
-					version: "6dc7e46bc9124c5c8824be4822abe105",
-					type: "visual-detector",
-				},
-				this.state.input
-			)
+		// app.models
+		// 	.predict(
+		// 		{
+		// 			id: "face-detection",
+		// 			name: "face-detection",
+		// 			version: "6dc7e46bc9124c5c8824be4822abe105",
+		// 			type: "visual-detector",
+		// 		},
+		// 		this.state.input
+		// 	)
+		fetch("http://localhost:3000/imageurl", {
+			method: "post",
+			headers: { "Content-Type": "application/json" },
+			body: JSON.stringify({
+				input: this.state.input,
+			}),
+		})
+			.then((response) => response.json())
 			.then((response) => {
 				if (response) {
 					fetch("http://localhost:3000/image", {
@@ -97,8 +116,10 @@ class App extends Component {
 						.then((response) => response.json())
 						.then((count) => {
 							this.setState(Object.assign(this.state.user, { entries: count }));
-						});
+						})
+						.catch(console.log);
 				}
+				
 				this.displayFaceBox(this.calculateFaceLocation(response));
 			})
 			.catch((err) => console.log(err));
@@ -106,7 +127,7 @@ class App extends Component {
 
 	onRouteChange = (route) => {
 		if (route === "signout") {
-			this.setState({ isSignedIn: false });
+			this.setState(initialState);
 		} else if (route === "home") {
 			this.setState({ isSignedIn: true });
 		}
--- src/components/FaceRecognition/FaceRecognition.js
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -2,14 +2,48 @@ import React from "react";
 import "./FaceRecognition.css";
 
 const FaceRecognition = ({ imageUrl, box }) => {
-	return (
-		<div className="center image-container">
-			<div className="box">
-				<img id="inputImage" src={imageUrl} alt="" className="image"/>
-				<div className="bounding-box" style={{top: box.topRow, right: box.rightCol, bottom: box.bottomRow, left: box.leftCol}}></div>
+	if (Array.isArray(box)) {
+		return (
+			<>
+				<div className="center image-container">
+					<div className="box">
+						<img id="inputImage" src={imageUrl} alt="" className="image" />
+						{box.map((item, index) => {
+							return (
+								<div
+									key={index}
+									className="bounding-box"
+									style={{
+										top: item.topRow,
+										right: item.rightCol,
+										bottom: item.bottomRow,
+										left: item.leftCol,
+									}}
+								></div>
+							);
+						})}
+					</div>
+				</div>
+			</>
+		);
+	} else {
+		return (
+			<div className="center image-container">
+				<div className="box">
+					<img id="inputImage" src={imageUrl} alt="" className="image" />
+					<div
+						className="bounding-box"
+						style={{
+							top: box.topRow,
+							right: box.rightCol,
+							bottom: box.bottomRow,
+							left: box.leftCol,
+						}}
+					></div>
+				</div>
 			</div>
-		</div>
-	);
+		);
+	}
 };
 
 export default FaceRecognition;
